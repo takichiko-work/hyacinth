f376f18276485fbfa51d424a56e26fc499c487d9
takichiko-work <watanabe_chihiro@zeromedical.co.jp>
2024-10-29 18:51:10 +0900

    APIキーをenv

--- libs/client.js
+++ libs/client.js
@@ -2,5 +2,5 @@ import { createClient } from 'microcms-js-sdk';
 
 export const client = createClient({
   serviceDomain: 'f8zohmep5h',
-  apiKey: 'GiB3po81lgPEyAs8sExdF3jZLL8c86JULu9x',
+  apiKey: process.env.API_KEY,
 });
